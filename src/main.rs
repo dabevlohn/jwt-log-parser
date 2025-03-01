47f508f07b27fc15ff38b994c61f602f21044772
Aran Varun <vir@tue.su>
2025-03-01 22:52:04 +0300

    prints plain claims

--- src/main.rs
+++ src/main.rs
@@ -32,46 +32,43 @@ async fn get_log() -> Result<String, reqwest::Error> {
 }
 
 /// Parse log file strings into JWT head requests.
-fn parse_log_strings(log_text: &str) -> Vec<JwtHeadRequest> {
-    let re = Regex::new(r"\[(.*)\]\s.HEAD\s(.*)\?jwt=(.*)\sHTTP/\d\.\d.\s\d{3}\s-").unwrap();
-    let mut requests = Vec::new();
+fn parse_log_strings(log_text: &str) -> Result<Vec<JwtHeadRequest>, regex::Error> {
+    let re = Regex::new(r"\[(.*)\]\s.HEAD\s(.*)\?jwt=(.*)\sHTTP/\d\.\d.\s\d{3}\s-")?;
+    let mut requests: Vec<JwtHeadRequest> = Vec::new();
 
     for line in log_text.lines().filter(|line| re.is_match(line)) {
-        let cap = re.captures(&line).unwrap();
-        requests.push(JwtHeadRequest {
-            timestamp: cap[1].to_string(),
-            path: cap[2].to_string(),
-            jwt: cap[3].to_string(),
-        });
+        if let Some(cap) = re.captures(&line) {
+            requests.push(JwtHeadRequest {
+                timestamp: cap[1].to_string(),
+                path: cap[2].to_string(),
+                jwt: cap[3].to_string(),
+            });
+        }
     }
 
-    return requests;
+    Ok(requests)
 }
 
 /// Validate JWT token.
-fn validate_token(token: &str) -> String {
+fn validate_token(token: &str) -> Result<Claims, jsonwebtoken::errors::Error> {
     let mut validation = jsonwebtoken::Validation::new(jsonwebtoken::Algorithm::HS256);
     validation.set_audience(&vec![AUDIENCE]);
     //validation.required_spec_claims = HashSet::new();
     //validation.validate_aud = false;
 
     let secret = jsonwebtoken::DecodingKey::from_secret(SECRET.as_bytes());
-    let res = jsonwebtoken::decode::<Claims>(&token, &secret, &validation);
-    format!("{:?}", res.unwrap().claims)
+    let res = jsonwebtoken::decode::<Claims>(&token, &secret, &validation)?;
+    Ok(res.claims)
 }
 
 /// Main.
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let log = get_log().await?;
-    let requests = parse_log_strings(&log);
-    for req in &requests {
-        println!("{:?}", req);
-
-        //validate_token(&req.jwt)
-        //    .parse::<Claims>()
-        //    .map(|claims| println!("{:?}", claims))
-        //    .unwrap();
+    let requests = parse_log_strings(&log)?;
+    for req in requests {
+        let claims = validate_token(&req.jwt)?;
+        println!("{:?}", claims);
     }
     Ok(())
 }
